2500705d094503da70466fb8bacad64276121248
George <gebretensai90@gmail.com>
2020-07-18 11:17:22 -0400

    Initial Commit

---
+++ SliderBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SliderBar
+//
+//  Created by George on 7/18/20.
+//  Copyright © 2020 George. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
